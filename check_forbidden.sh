06deadd7bde98a667ddb58dbd6b24c479bef8d0e
Moe-Salim91156 <hmodaisleem88@gmail.com>
2025-07-29 01:08:57 +0300

    examples on [] [[]]

---
+++ check_forbidden.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+forbidden_funcs=("printf" "execve")
+
+for func in "${forbidden_funcs[@]}"; do
+    echo "Checking for: $func"
+    grep -rnw . -e "$func" | grep allowed
+done
---
+++ git_helper.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# git_helper.sh - Interactive git helper for common tasks
+
+while true; do
+    echo "Git Helper Menu:"
+    echo "1) Status"
+    echo "2) Add all"
+    echo "3) Commit"
+    echo "4) Push"
+    echo "5) Pull"
+    echo "6) Branches"
+    echo "7) Exit"
+    read -p "Choose an option [1-7]: " choice
+
+    case $choice in
+        1) git status ;;
+        2) git add .; echo "Added all changes." ;;
+        3) read -p "Commit message: " msg; git commit -m "$msg" ;;
+        4) git push ;;
+        5) git pull ;;
+        6) git branch -a ;;
+        7) echo "Bye!"; exit 0 ;;
+        *) echo "Invalid option." ;;
+    esac
+    echo
+done
+
+
